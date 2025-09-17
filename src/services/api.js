2e93aa26c34f168c249e14e0ec410a197f82cc71
sebastian <sebastianbill06@gmail.com>
2025-09-17 17:59:23 +0300

    backend url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:5000/api";
+const API_URL = "https://oneofwun-db.onrender.com";
 
 export const API = axios.create({
   baseURL: API_URL,
