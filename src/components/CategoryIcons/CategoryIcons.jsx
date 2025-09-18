c7b52a0a1a3a8cac66b99ae536e45634158d58b4
sebastian <sebastianbill06@gmail.com>
2025-09-18 14:16:11 +0300

    link category icons

--- src/components/CategoryIcons/CategoryIcons.jsx
+++ src/components/CategoryIcons/CategoryIcons.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './CategoryIcons.css';
 import { PiDiamond, PiSunglasses, PiHandbag, PiBaseballCap } from 'react-icons/pi';
 
+const shopUrl = '/shop'; 
+
 const categories = [
   { name: 'Jewelry', icon: <PiDiamond /> },
   { name: 'Sunglasses', icon: <PiSunglasses /> },
   { name: 'Bags', icon: <PiHandbag /> },
-  { name: 'Beanies', icon: <PiBaseballCap /> } 
+  { name: 'Hats', icon: <PiBaseballCap /> } 
 ];
 
 const CategoryIcons = () => {
@@ -15,12 +17,12 @@ const CategoryIcons = () => {
       <h2 className="section-title">Shop by Category</h2>
       <div className="category-icons">
         {categories.map((cat, index) => (
-          <div key={index} className="category-item">
+          <a href={shopUrl} key={index} className="category-item">
             <div className="icon-container">
               {cat.icon}
             </div>
             <p className="category-name">{cat.name}</p>
-          </div>
+          </a>
         ))}
       </div>
     </section>
